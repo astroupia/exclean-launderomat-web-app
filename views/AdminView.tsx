a2a4bec92ff51b372bd4042cff9900221522557b
Nahom Tegaude <nahomteguade@gmail.com>
2024-08-15 11:22:01 +0300

    Setting Up Views

---
+++ views/AdminView.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminView = () => {
+  return <div></div>;
+};
+
+export default AdminView;
---
+++ views/CustomerView.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomerView = () => {
+  return <div></div>;
+};
+
+export default CustomerView;
