6f22d4239f53258ffb165bd2a6624a6a9612466b
Nahom Tegaude <nahomteguade@gmail.com>
2024-08-27 21:23:55 +0300

    Param Type Setup

---
+++ types/index.ts
@@ -0,0 +1,12 @@
+export type CreateUserPramas = {
+  clerkId: String;
+  email: String;
+  role: String;
+  firstName: String;
+  lastName: String;
+};
+
+export type UpdateUserPramas = {
+  firstName: String;
+  lastName: String;
+};
