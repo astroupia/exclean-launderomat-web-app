d7a53c013178e55cdd0320cd621caca8772340c3
Nahom Tegaude <nahomteguade@gmail.com>
2024-08-29 22:09:27 +0300

    User Auth Fixed

--- models/user.ts
+++ models/user.ts
@@ -4,6 +4,7 @@ const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   role: { type: String, required: true, default: "Customer" },
+  username: { type: String },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
 });
