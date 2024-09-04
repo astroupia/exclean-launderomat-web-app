388fce93849dc6e9943d089dc7118b48291fa8a4
Nahom Tegaude <nahomteguade@gmail.com>
2024-09-04 22:09:49 +0300

    Next Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // Add any other Next.js config options here
+  env: {
+    MONGODB_URL: process.env.MONGODB_URL,
+  },
+  // ... other configurations
 };
 
-module.exports = nextConfig;
+export default nextConfig;
