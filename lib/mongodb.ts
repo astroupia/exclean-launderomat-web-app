88e35436babe652849866b05044d62981e50ec89
Nahom Tegaude <nahomteguade@gmail.com>
2024-09-05 13:42:08 +0300

    Mongo DB Connection Type Fixed

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,4 +1,4 @@
-import { Db } from "mongodb";
+import { Db, MongoClient } from "mongodb";
 
 let cachedDb: Db | null = null;
 
@@ -6,10 +6,19 @@ export async function getDatabase(): Promise<Db> {
   if (cachedDb) {
     return cachedDb;
   }
-
   const { connectToDatabase } = await import("../utils/database");
-  const client = await connectToDatabase();
-  const db = client.db(process.env.MONGODB_DB);
+  const connection = await connectToDatabase();
+  if (!connection) {
+    throw new Error("Failed to connect to the database");
+  }
+  const client = connection as unknown as MongoClient;
+
+  const dbName = process.env.MONGODB_DB;
+  if (!dbName) {
+    throw new Error("MONGODB_DB environment variable is not set");
+  }
+
+  const db = client.db(dbName);
   cachedDb = db;
   return db;
 }
